5550b93fa71b262578d18369b61f0a84fb9b36d6
Sebastian Mendel <sebastian.mendel@netresearch.de>
2024-10-18 15:39:57 +0200

    add vue head

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,6 @@
-import './bootstrap';
 import { createApp } from 'vue';
 import ElementPlus from 'element-plus';
-import 'element-plus/lib/theme-chalk/index.css';
+//import 'element-plus/lib/theme-chalk/index.css';
 import App from './components/App.vue';
 
 const app = createApp(App);
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import Components from 'unplugin-vue-components/vite';
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
+import AutoImport from 'unplugin-auto-import/vite';
 
 export default defineConfig({
     plugins: [
@@ -9,5 +12,18 @@ export default defineConfig({
             refresh: true,
         }),
         vue(),
+        Components({
+            resolvers: [ElementPlusResolver()],
+        }),
+        AutoImport({
+            resolvers: [ElementPlusResolver()],
+        }),
     ],
+    build: {
+        manifest: true,
+        outDir: 'public/build',
+        rollupOptions: {
+            input: 'resources/js/app.js',
+        },
+    },
 });
